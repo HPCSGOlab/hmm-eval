e1d04bae6a1a9b588ae0842b721dc7bb245e5c9f
Tyler Allen <t.allen@charlotte.edu>
2023-09-16 17:31:30 -0400

    fixing some of the perf scripting

--- benchmarks/basic/perf.sh
+++ benchmarks/basic/perf.sh
@@ -4,9 +4,12 @@ exe="./basic"
 N=(1000000000 2000000000 3000000000 4000000000 5000000000 6000000000)
 perfdir=perf/
 mkdir -p ${perfdir}
+driver="NVIDIA-Linux-x86_64-535.104.05"
+path="/home/tallen93/"
+
 
 sudo systemctl stop nvidia-persistenced.service
-sudo /home/tallen93/cuda_12.2.0_535.54.03_linux.run --silent --driver -m=kernel
+sudo ${path}/${driver} --silent --driver -m=kernel
 sudo systemctl start nvidia-persistenced.service
 
 prefix="closed"
@@ -18,7 +21,7 @@ for i in ${N[@]}; do
 done
 
 sudo systemctl stop nvidia-persistenced.service
-sudo /home/tallen93/cuda_12.2.0_535.54.03_linux.run --silent --driver -m=kernel-open 
+sudo ${path}/${driver} --silent --driver -m=kernel-open 
 sudo systemctl start nvidia-persistenced.service
 
 prefix="open"
@@ -30,7 +33,7 @@ for i in ${N[@]}; do
 done
 
 sudo systemctl stop nvidia-persistenced.service
-sudo /home/tallen93/cuda_12.2.0_535.54.03_linux.run --silent --driver -m=kernel-open
+sudo ${path}/${driver} --silent --driver -m=kernel-open
 sudo systemctl start nvidia-persistenced.service
 
 prefix="hmm"
