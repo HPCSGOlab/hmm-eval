bd78ec712c1fdb31122d54ab5e51d70d07b36d15
Nicholas Cassarino <ncassari@cci-hopper2.charlotte.edu>
2024-12-11 16:32:52 -0500

    scripts for flamegraph on sgemm

---
+++ experiments/flamegraph/sgemm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -xe
+
+module load cuda
+
+types=("base" "uvm" "hmm")
+
+rm -rf ../../figs/flamegraph/`hostname`/
+
+mkdir -p ../../figs/flamegraph/`hostname`/
+
+
+for N in ${types[@]}; do
+	cd ../../benchmarks/apps/$N/sgemm
+	make
+
+	cd ../../../../experiments/flamegraph/FlameGraph
+
+	perf record -F 99 -a -g ./../../../benchmarks/apps/$N/sgemm/sgemm -n 65000
+	perf script | ./stackcollapse-perf.pl > out.perf-folded
+	./flamegraph.pl out.perf-folded > sgemm_$N.svg
+	mv sgemm_$N.svg ../../../figs/flamegraph/`hostname`/
+
+	cd ../
+done
