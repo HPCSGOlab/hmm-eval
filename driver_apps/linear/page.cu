ebcbdf3aba035c1a491d11aa632c6998f10b7b44
Nicholas Cassarino <nmcassa804@outlook.com>
2025-02-04 14:39:31 -0500

    made the linear benchmark HMM instead of UVM

--- driver_apps/linear/page.cu
+++ driver_apps/linear/page.cu
@@ -152,7 +152,8 @@ int main(int argc, char* argv[])
     {
         CHECK_CUDA_ERROR();
         printf("Allocating %f mb\n", ARRAY_SIZE * sizeof(float) / 1000000.);
-        cudaMallocManaged(&array, ARRAY_SIZE * sizeof(float));
+        //cudaMallocManaged(&array, ARRAY_SIZE * sizeof(float));
+	array = (float *) malloc(ARRAY_SIZE * sizeof(float));
         CHECK_CUDA_ERROR();
         printf("alloced,%ld\n", ARRAY_SIZE * sizeof(float));
 
@@ -196,7 +197,8 @@ int main(int argc, char* argv[])
         //printf("perf,%lf\n", msecTotal/1000.0);
 
     CHECK_CUDA_ERROR();
-        cudaFree(array);
+        //cudaFree(array);
+    	free(array);
     CHECK_CUDA_ERROR();
     }
 }
